fed29ba84fa5e9a0eb7935dd88233cb6dd0492ec
Brian Smith <briansmith.work578@gmail.com>
2023-07-15 22:15:18 +0100

    started exlir learning

---
+++ playground/cars/lib/cars.ex
@@ -0,0 +1,32 @@
+defmodule Cars do
+  @moduledoc """
+  Documentation for `Cars`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cars.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  @doc """
+  `create_zoo/0` returns a list of zoo animals
+
+  ## Examples
+
+      iex> Animals.create_zoo
+      ["lion", "tiger", "gorilla", "elephant", "monkey", "giraffe"]
+  """
+
+  def create_garage do
+    ["BMW", "Mercedes", "Audi", "VW", "Porsche", "Opel"]
+  end
+
+end
